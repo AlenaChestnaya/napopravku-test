4fa95fa5a8fbb37e55df9a91b8ec2c5724a4df0f
Alena <chestnaja_alena@mail.ru>
2020-04-19 14:38:36 +0300

    добавила скрипты для открытия/закрытия попапа

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function () {
+
+    var ESC_CODE = 27;
+    var pointsButton = document.querySelector('.points-button');
+    var overlay = document.querySelector('.overlay');
+    var popup = document.querySelector('.popup');
+    var сlose = popup.querySelector('.popup__close');
+
+    var showPopup = function() {
+        popup.classList.add('popup--show');
+        overlay.classList.add('overlay--show');
+    };
+
+    var closePopup = function() {
+        if (popup.classList.contains('popup--show')) {
+            popup.classList.remove('popup--show');
+            overlay.classList.remove('overlay--show');
+        }
+    };
+    
+    pointsButton.addEventListener('click', showPopup);
+    сlose.addEventListener('click', closePopup);
+    overlay.addEventListener('click', closePopup);
+    window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === ESC_CODE) {
+            closePopup();
+        }
+    });
+})();
